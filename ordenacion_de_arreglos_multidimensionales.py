b8211f7261a1a76473768905e391bff97c4cdd52
Johao A. Caicedo <ja.caicedob@uea.edu.ec>
2025-03-02 14:23:32 -0500

    Se colocaron comentarios para poder comprender las instrucciones para realizar el ejercicio, además de especifica primero la matriz y luego la función, tambien se cambió el nombre de la función.

--- ordenacion_de_arreglos_multidimensionales.py
+++ ordenacion_de_arreglos_multidimensionales.py
@@ -1,9 +1,8 @@
-def bubble_sort(fila):
-    n = len(fila)
-    for i in range(n):
-        for j in range(0, n - i - 1):
-            if fila[j] > fila[j + 1]:
-                fila[j], fila[j + 1] = fila[j + 1], fila[j]  # Intercambiar elementos
+#Programa 2: Ordenación de Arreglo Multidimensional
+#Crea un nuevo proyecto en PyCharm y un archivo Python para este programa.
+#Escribe un programa que incluya una matriz bidimensional con valores numéricos (puede ser una matriz pequeña de 3x3).
+#Implementa una función que ordene una fila específica de la matriz en orden ascendente utilizando un algoritmo de ordenación de tu elección (por ejemplo, Bubble Sort o QuickSort).
+#Muestra la matriz original y la matriz con la fila ordenada.
 
 # Establecer los valores de la matriz 3x3
 matriz_ordenacion = [
@@ -12,6 +11,14 @@ matriz_ordenacion = [
     [27, 21, 24]
 ]
 
+def ordenacion(fila):
+    n = len(fila)
+    for i in range(n):
+        for j in range(0, n - i - 1):
+            if fila[j] > fila[j + 1]:
+                fila[j], fila[j + 1] = fila[j + 1], fila[j]  # Intercambiar elementos
+
+
 # Presentar los datos de la matriz original
 print("Matriz original:")
 for fila in matriz_ordenacion:
@@ -19,7 +26,7 @@ for fila in matriz_ordenacion:
 
 # Seleccionar la fila a ordenar
 fila_a_ordenar = 0
-bubble_sort(matriz_ordenacion[fila_a_ordenar])
+ordenacion(matriz_ordenacion[fila_a_ordenar])
 
 # Presentar la matriz luego de la ordenación
 print("\nMatriz después de ordenar la fila 2:")
