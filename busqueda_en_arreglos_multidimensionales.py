181352fc5ca2704b1bfdf3866cf691dbb728bf23
Johao A. Caicedo <ja.caicedob@uea.edu.ec>
2025-03-02 14:19:43 -0500

    Se colocaron comentarios para poder comprender las instrucciones para realizar el ejercicio, además de especifica primero la matriz y luego la función

--- busqueda_en_arreglos_multidimensionales.py
+++ busqueda_en_arreglos_multidimensionales.py
@@ -1,9 +1,9 @@
-def busqueda (matriz_busqueda, valor):
-    for i in range(len(matriz_busqueda)):
-        for j in range(len(matriz_busqueda[i])):
-            if matriz_busqueda[i][j] == valor:
-                return i, j  # Retorna la posición (fila, columna)
-    return None
+#Programa 1: Búsqueda en Arreglo Multidimensional
+#Instrucciones
+#Crea un nuevo proyecto en PyCharm y un archivo Python para este programa.
+#Escribe un programa que incluya una matriz bidimensional (puede ser una matriz pequeña de 3x3) con valores numéricos.
+#Implementa una función que realice una búsqueda en la matriz para encontrar un valor específico que definas.
+#Muestra un mensaje que indique si el valor se encontró o no, y muestra su posición en la matriz si se encontró.
 
 # Establecer la matriz de búsqueda 3x3
 matriz_busqueda = [
@@ -11,6 +11,13 @@ matriz_busqueda = [
     [8, 10, 12],
     [14, 16, 18]
 ]
+
+def busqueda (matriz_busqueda, valor):
+    for i in range(len(matriz_busqueda)):
+        for j in range(len(matriz_busqueda[i])):
+            if matriz_busqueda[i][j] == valor:
+                return i, j  # Retorna la posición (fila, columna)
+    return None
 
 # valor que se buscará en la matriz
 valor_a_buscar = int(input("Digite el valor que desea buscar: "))
